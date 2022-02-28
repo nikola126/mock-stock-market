e4a93515acf36f18adcf75965180762cacb6a72d
Nikola <nikj1705@gmail.com>
2022-02-28 14:07:21 +0200

    CORS properties added

---
+++ src/main/java/com/stock/backend/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.stock.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origins:[]}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**").allowedOrigins(allowedOrigins);
+    }
+}
