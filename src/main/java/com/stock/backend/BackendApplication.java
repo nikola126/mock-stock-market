8547e8f61bf1036efcc5a2cddfe01d966540df00
Nikola <nikj1705@gmail.com>
2022-02-28 17:54:59 +0200

    CORS properties added

--- src/main/java/com/stock/backend/BackendApplication.java
+++ src/main/java/com/stock/backend/BackendApplication.java
@@ -2,10 +2,7 @@ package com.stock.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,17 +11,4 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000","http://localhost:3001")
-                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
-            }
-        };
-    }
-
 }
